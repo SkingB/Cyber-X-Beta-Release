527eae79127cbe0ef3ad5c040de9b0555c839b7c
SkingB <125189513+SkingB@users.noreply.github.com>
2023-03-25 19:30:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,12 +17,12 @@ global.APIKeys = {
 
 //AUTO FUNCTIONS ( 📵 DONT EDIT 📵 ) ITS FOR NEXT UPDATE
 
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autobio = false  // auto bio system checking on Developing Leval
 global.autoRecord = false //auto recording (true to on, false to off)
-global.available = true //auto available (true to on, false to off)
+global.available =true //auto available (true to on, false to off)
 
 //VIDEO MENU 
 global.vidmenu = fs.readFileSync("./LocalData/theme/cyber.mp4") //gif and video menu
@@ -36,26 +36,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['94706785285'] //ur owner number
+global.ownername = "SkingB BOT" //ur owner name
+global.ytname = "YT: SkinB" //ur yt chanel name
+global.socialm = "GitHub:" //ur github or insta name
+global.location = "Sri Lanka, negombo,dankotuva" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['94706785285']
+global.ownertag = '94706785285' //ur tag number
+global.botnumber = '94706785285' //Add your Bot Number For Log
+global.botname = 'SkingB BOT' //ur bot name
+global.ig = "https://www.youtube.com/@skingb" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/@skingb" //ur website to be displayed
+global.sc = 'https://www.youtube.com/@skingb' //script link
+global.linkz = 'https://www.youtube.com/@skingb'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "SkingB BOT" //ur sticker watermark packname
+global.author = "SkingB BOT" //ur sticker watermark author
+global.footer = "SkingB BOT"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94706785285'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
